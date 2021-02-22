6aabacdcea24095d7676698f7bb7df2f41b656fe
ThansiJamal <thansijamal18@gmail.com>
2021-02-21 18:39:39 -0800

    first commit

---
+++ ProgressBarSample/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ProgressBarSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tmrTime = new System.Windows.Forms.Timer(this.components);
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.tmrTime2 = new System.Windows.Forms.Timer(this.components);
+            this.tmrTime3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(30, 54);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(275, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // tmrTime
+            // 
+            this.tmrTime.Enabled = true;
+            this.tmrTime.Tick += new System.EventHandler(this.tmrTime_Tick);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(198, 113);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(275, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(369, 54);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(275, 23);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // tmrTime2
+            // 
+            this.tmrTime2.Enabled = true;
+            this.tmrTime2.Tick += new System.EventHandler(this.tmrTime2_Tick);
+            // 
+            // tmrTime3
+            // 
+            this.tmrTime3.Enabled = true;
+            this.tmrTime3.Tick += new System.EventHandler(this.tmrTime3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 174);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Progress Bar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Timer tmrTime2;
+        public System.Windows.Forms.Timer tmrTime;
+        private System.Windows.Forms.Timer tmrTime3;
+    }
+}
+
---
+++ ProgressBarSample/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBarSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrTime_Tick(object sender, EventArgs e)
+        {
+                if (progressBar1.Value == 50)
+                {
+                    tmrTime.Enabled = false;
+                }
+                progressBar1.Value += 2;
+            tmrTime2.Start();
+        }
+
+        private void tmrTime2_Tick(object sender, EventArgs e)
+        {
+
+            if (tmrTime.Enabled==false)
+            {
+                if (progressBar2.Value == 50)
+                {
+                    tmrTime2.Enabled = false;
+                }
+                progressBar2.Value += 2;
+            }
+            tmrTime3.Start();
+        }
+
+        private void tmrTime3_Tick(object sender, EventArgs e)
+        {
+            if(tmrTime2.Enabled==false)
+            {
+                if (progressBar3.Value == 95)
+                {
+                    tmrTime3.Enabled = false;
+                }
+                progressBar3.Value += 5;
+
+            }
+        }
+    }
+}
